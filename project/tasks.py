4aa16ef75bed867ce5ba1138d30cb8e399b1f0b4
Timothée Mazzucotelli <pawamoy@pm.me>
2020-09-04 18:27:51 +0200

    Exclude tests/fixtures from formatting commands

--- project/tasks.py
+++ project/tasks.py
@@ -254,7 +254,10 @@ def format(context):  # noqa: W0622 (we don't mind shadowing the format builtin)
     Arguments:
         context: The context of the Invoke task.
     """
-    context.run("failprint -t 'Removing unused imports' -- autoflake -ir --remove-all-unused-imports " + PY_SRC)
+    context.run(
+        "failprint -t 'Removing unused imports' -- "
+        "autoflake -ir --exclude tests/fixtures --remove-all-unused-imports " + PY_SRC
+    )
     context.run("failprint -t 'Ordering imports' -- isort -y -rc " + PY_SRC)
     context.run("failprint -t 'Formatting code' -- black " + PY_SRC)
 
