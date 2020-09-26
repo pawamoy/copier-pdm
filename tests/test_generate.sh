af84a2a2e85625f48d6db84a4c80088707052834
Timothée Mazzucotelli <pawamoy@pm.me>
2020-09-26 16:12:18 +0200

    Increase template tests verbosity

--- tests/test_generate.sh
+++ tests/test_generate.sh
@@ -24,14 +24,30 @@ echo "///////////////////////////////////////////"
 echo "             TESTING PROJECT"
 echo "///////////////////////////////////////////"
 echo
+echo ">>> Creating initial commit (feat)"
 git add -A .
 git commit -am "feat: Initial commit"
 git tag v0.1.0
 echo
-make --no-print-directory setup check
-make --no-print-directory docs-regen format check test
+echo ">>> Setting up Python environments"
+make --no-print-directory setup
+echo
+echo ">>> Running initial quality checks"
+make --no-print-directory check
+echo
+echo ">>> Generating docs, formatting, and re-running quality checks"
+make --no-print-directory docs-regen format check
+echo
+echo ">>> Running tests"
+make --no-print-directory test
+echo
+echo ">>> Creating second commit (fix)"
 git commit -am "fix: Fix all bugs"
+echo
+echo ">>> Updating changelog and releasing version"
 poetry run invoke changelog release -v 0.1.1
+echo
+echo ">>> Checking changelog's contents"
 poetry run failprint -- grep 'v0\.1\.0' CHANGELOG.md
 poetry run failprint -- grep 'v0\.1\.1' CHANGELOG.md
 poetry run failprint -- grep 'Features' CHANGELOG.md
