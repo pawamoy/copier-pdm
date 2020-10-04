0dc46712906158f40077aa499be648d51d909654
Timothée Mazzucotelli <pawamoy@pm.me>
2020-10-04 16:12:19 +0200

    Refactor to use duty

--- project/scripts/multirun.sh
+++ project/scripts/multirun.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+PYTHON_VERSIONS="${PYTHON_VERSIONS:-3.6 3.7 3.8}"
+
 if [ -n "${PYTHON_VERSIONS}" ]; then
     for python_version in ${PYTHON_VERSIONS}; do
         if output=$(poetry env use "${python_version}" 2>&1); then
@@ -8,13 +10,13 @@ if [ -n "${PYTHON_VERSIONS}" ]; then
                 echo "> Environment for Python ${python_version} not created, skipping" >&2
                 poetry env remove "${python_version}" &>/dev/null || true
             else
-                echo "> $@ (Python ${python_version})"
-                "$@"
+                echo "> poetry run $@ (Python ${python_version})"
+                poetry run "$@"
             fi
         else
             echo "> poetry env use ${python_version}: Python version not available?" >&2
         fi
     done
 else
-    "$@"
+    poetry run "$@"
 fi
