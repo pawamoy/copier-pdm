ea5c8b97c8b9cca548f561a27287de447c35cfe2
Timothée Mazzucotelli <pawamoy@pm.me>
2021-03-12 23:21:08 +0100

    tests: Coverage report is automatically faked by check-docs now

--- tests/test_generate.sh
+++ tests/test_generate.sh
@@ -43,10 +43,6 @@ git add -A .
 git commit -am "feat: Initial commit"
 git tag v0.1.0
 echo
-echo ">>> Faking coverage HTML report"
-mkdir -p build/coverage
-touch build/coverage/index.html
-echo
 echo ">>> Setting up Python environments"
 make --no-print-directory setup
 echo
