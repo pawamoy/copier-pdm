05f91791a41fe8e1ff4696ca2a3374727691fa65
Shawn Smith <ssmith161803@gmail.com>
2023-11-08 21:54:32 -0700

    refactor(paths): updated test repo

--- reset-history.sh
+++ reset-history.sh
@@ -2,7 +2,7 @@
 cd tests/tmp || exit 1
 rm -rf .git
 git init .
-git remote add origin git@github.com:pawamoy/pawamoy-testing
+git remote add origin git@github.com:sl-smith/sl-smith-testing
 git add -A
 git commit -m "initial commit"
 git push origin master -f
